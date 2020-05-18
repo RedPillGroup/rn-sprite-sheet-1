424756e0c6168a226bab19fdf150cdb04de7d474
alexandre <alexandre@redpill.paris>
2020-05-18 15:00:47 +0200

    Add verif use delegate

--- ios/RNSpriteSheet/AppDelegate.h
+++ ios/RNSpriteSheet/AppDelegate.h
@@ -5,7 +5,11 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#import <React/RCTBridgeDelegate.h>
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
+#endif
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
